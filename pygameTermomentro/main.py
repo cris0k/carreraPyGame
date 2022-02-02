bbcf1e68355261e4b07d0ad7f13e80714325fc14
cristina <criskukhar@hotmail.com>
2022-02-02 17:30:29 +0100

    Imagen fondo termometro

--- pygameTermomentro/main.py
+++ pygameTermomentro/main.py
@@ -1,6 +1,10 @@
 import pygame, sys
 from pygame.locals import *
 
+class Termometro():
+    def __init__(self):
+        self.custome = pygame.image.load("images/termo1.png")
+
 class mainApp():
     termometro = None
     entrada = None
@@ -11,6 +15,8 @@ class mainApp():
         pygame.display.set_caption("Termomentro")
         self.__screen.fill((244,236,203))
         
+        self.termometro = Termometro()
+        
     def __on_close(self):
         pygame.quit()
         sys.exit()
@@ -20,7 +26,8 @@ class mainApp():
             for event in pygame.event.get():
                 if event.type == QUIT:
                     self.__on_close()
-                    
+            
+            self.__screen.blit(self.termometro.custome,(50,34))        
             pygame.display.flip()
         
         
